f57a076bf37074e9d6f1d8c296203f22a0d2282b
Shun Kakinoki <shunkakinoki@gmail.com>
2019-10-25 22:13:26 +0900

    adding proper entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,12 +27,6 @@ if [ -z "$MD_FOLDER" ]; then
   MD_FOLDER='.'
 fi
 
-if [ ! -z "$SKIP_MD" ]; then
-  DOC_TO_SKIP=(`echo $SKIP_MD | sed s/,/\ /g`)
-else
-  DOC_TO_SKIP=
-fi
-
 mkdir $TMP_CLONE_FOLDER
 cd $TMP_CLONE_FOLDER
 git init
@@ -41,14 +35,7 @@ git config user.email $ACTION_MAIL
 git pull https://${GH_PAT}@github.com/$OWNER/$REPO_NAME.wiki.git
 cd ..
 
-for i in $(find $MD_FOLDER -maxdepth 3 -type f -name '*.md' -execdir basename '{}' ';'); do
-    echo $i
-    if [[ ! " ${DOC_TO_SKIP[@]} " =~ " ${i} " ]]; then
-        cp $MD_FOLDER/$i $TMP_CLONE_FOLDER
-    else
-        echo "Skipping $i as it matches the $SKIP_MD rule"
-    fi
-done
+for i in "$MD_FOLDER"/*.md ; do cp "$i" "$TMP_CLONE_FOLDER"; done
 
 echo "Pushing new pages"
 cd $TMP_CLONE_FOLDER
